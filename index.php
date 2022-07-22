c7c7ac5ed1014b1cc13b37d09ae1a938e47797e9
CleberSoares77 <109772121+CleberSoares77@users.noreply.github.com>
2022-07-21 22:24:30 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  color: black;
+}
+
+h1 {
+  color: green;
+}
+</style>
+</head>
+<body>
+
+<h1>Site Informativo</h1>
+<p>Olá aluno(a), seja bem vindo(a)</p>
+<p>Diante da evasão e trancamento de curso, os discentes do curso de Análise e Desenvolvimento de Sistemas necessitam de informações prévias e específicas sobre o curso</p>
+<p>Com esse propósito a intenção deste site e justamente te informar sobre o que o curso e oferecer a você programador um belo de um "Spoiler"...</p>
+<p>Antes de informamos a você, inserimos um formulário para dizer sobre suas expectativas com o curso, Vamos lá?</p>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Formulário</h2>
+
+<p>Qual destas linguagens de programação você gostaria de aprender:</p>
+
+<form>
+  <input type="radio" id="html" name="fav_language" value="HTML">
+  <label for="html">HTML</label>
+  <input type="radio" id="css" name="fav_language" value="CSS">
+  <label for="css">CSS</label>
+  <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+  <label for="javascript">JavaScript</label>
+  <input type="radio" id="php" name="fav_language" value="PHP">
+  <label for="php">PHP</label>
+  <input type="radio" id="c++" name="fav_language" value="C++">
+  <label for="c++">C++</label>
+  <input type="radio" id="c#" name="fav_language" value="C#">
+  <label for="c#">C#</label>
+  <input type="radio" id="java" name="fav_language" value="JAVA">
+  <label for="java">JAVA</label>
+  <input type="radio" id="python" name="fav_language" value="PYTHON">
+  <label for="python">PYTHON</label>
+  <input type="radio" id="sql" name="fav_language" value="SQL">
+  <label for="sql">SQL</label>
+  <input type="radio" id="r" name="fav_language" value="R">
+  <label for="r">R</label>
+  <input type="radio" id="vba" name="fav_language" value="VBA">
+  <label for="vba">VBA</label><br>
+  <input type="radio" id="typescript" name="fav_language" value="TypeScript">
+  <label for="typescript">TypeScript</label>
+  <input type="radio" id="ruby" name="fav_language" value="RUBY">
+  <label for="ruby">RUBY</label>
+  <input type="radio" id="c" name="fav_language" value="C">
+  <label for="c">C</label>
+  <input type="radio" id="swift" name="fav_language" value="SWIFT">
+  <label for="swift">SWIFT</label>
+  <input type="radio" id="objetive-c" name="fav_language" value="Objective-C">
+  <label for="objective-c">Objective-C</label>
+
+</form> 
+
+</body>
+</html>
