9de6dca9c70e8ecd62043eab5d0d4968f88f431c
CleberSoares77 <109772121+CleberSoares77@users.noreply.github.com>
2022-08-15 18:36:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
-<!DOCTYPE HTML>
-<html lang="pt-br">
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 <style>
 body {
   color: black;
@@ -8,28 +13,107 @@ body {
 
 h1 {
   color: green;
- 
-  text-align:center;
-}
-</style>
-<meta charset="UTF-8">
-<title>Validação de formulários 2</title>
- 
-</head>
+  text-align: center;
 
+}
+
+h4 {
+  color: black;
+  text-align: center;
+
+}
+
+{
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+.header {
+  background-color: #f1f1f1;
+  padding: 20px;
+  text-align: center;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+
+</style>
+</head>
 <body>
 
-<h1>Site Informativo</h1>
-<hr />
-<p>Olá aluno(a), seja bem vindo(a)</p>
-<p>Diante da evasão e trancamento de curso, os discentes do curso de Análise e Desenvolvimento de Sistemas necessitam de informações prévias e específicas sobre o curso,
+<i class="fas fa-bars"></i>
+
+<div class="header">
+<h1>Visão Tecnológica</h1>
+</div>
+
+<div class="topnav">
+  <a href="#">Disciplinas</a>
+  <a href="#">Objetivos</a>
+  <a href="#">Comentários</a>
+</div>
+
+<h4>Olá aluno(a), seja bem vindo(a)</h4>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Diante da evasão e trancamento de curso, os discentes do curso de Análise e Desenvolvimento de Sistemas necessitam de informações prévias e específicas sobre o curso,
 com esse propósito a intenção deste site e justamente te informar sobre o que o curso e oferecer ao programador um belo de um "Spoiler", o curso de Análise e 
 de Sistemas possuí em sua grade curricular várias disciplinas que contemplam a prática e a teoria correlação, inicialmente, o curso possui duração de 3 anos,
-possibilita ao discente uma aprendizagem de alto nivel e capacitação para um mercado sempre em alta.</p>
+possibilita ao discente uma aprendizagem de alto nivel e capacitação para um mercado sempre em alta.
 
-<img src="vagasporarea_nift.jpg" alt="Trulli" width="500" height="333">
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
 
- 
+</body>
 </html>
- 
-</script>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+</body>
+</html>
+
+
